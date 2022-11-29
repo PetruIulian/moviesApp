26b178c71ce7ef38d095e8bed7133e7318209f9a
PetruIulian <petruiulian01@gmail.com>
2022-11-29 03:06:56 +0200

    formatting fixed

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,13 @@ class MoviesPage extends StatefulWidget {
 }
 
 class _MoviesPageState extends State<MoviesPage> {
-
   List<dynamic> _movies = <dynamic>[];
   bool _loading = true;
 
-  Future<void> getMovies() async{
+  Future<void> getMovies() async {
     final http.Response response = await http.get(Uri.parse('https://yts.torrentbay.to/api/v2/list_movies.json'));
-    if(response.statusCode == 200){
-      final  jsonData = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
       setState(() {
         _movies = [jsonData['data']['movies']];
         _loading = false;
@@ -49,86 +48,87 @@ class _MoviesPageState extends State<MoviesPage> {
     }
   }
 
-
   @override
   void initState() {
     super.initState();
     getMovies();
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        drawer: const Drawer(),
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: const Center(child: Text('Movies'),
-          ),
-          actions: <Widget>[IconButton(onPressed: (){}, icon: const Icon(Icons.search))],
-          elevation: 0,
-          backgroundColor: Colors.transparent,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const Drawer(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Movies'),
         ),
-        body: PageView.builder(itemBuilder: (BuildContext context, int index){
-            return DecoratedBox(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Colors.blueAccent,
-                      Colors.blue,
-                      Colors.blueGrey
+        actions: <Widget>[IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: PageView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return DecoratedBox(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[Colors.blueAccent, Colors.blue, Colors.blueGrey],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: _loading
+                ? const CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 300,
+                        height: 450,
+                        decoration: BoxDecoration(
+                          boxShadow: const <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5,
+                              offset: Offset(11, 10),
+                            )
+                          ],
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage('${_movies[0][index]['medium_cover_image']}'),
+                          ),
+                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 18),
+                        child: Center(
+                          child: Text(
+                            "${_movies[0][index]['title']}",
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 36,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "${_movies[0][index]['genres'][0]} | ${_movies[0][index]['rating']} / 10",
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white12,
+                          ),
+                        ),
+                      )
                     ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
                   ),
-                ),
-                child: _loading ?
-                const CircularProgressIndicator() :
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      height: 450,
-                      decoration: BoxDecoration(
-                        boxShadow: const <BoxShadow>[
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 5,
-                            offset: Offset(11,10)
-                          ,)
-                        ],
-                        image: DecorationImage(
-                            fit: BoxFit.cover, image: NetworkImage('${_movies[0][index]['medium_cover_image']}'),),
-                        borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 18),
-                      child: Center(
-                        child: Text("${_movies[0][index]['title']}",
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 36,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        "${_movies[0][index]['genres'][0]} | ${_movies[0][index]['rating']} / 10",
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white12
-                        ,),
-                      ),
-                    )
-                  ],
-                )
-            ,);
-        },)
-      ,);
-    }
+          );
+        },
+      ),
+    );
+  }
 }
